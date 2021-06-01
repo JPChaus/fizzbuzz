b1cb14a5cb29e94690f0ef57faec005112fb2f90
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-05-31 22:56:13 -0400

    wrote second test, testsing fizz output

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,5 +5,8 @@ class TestCase(unittest.TestCase):
     def test_printNum(self):
         self.assertEqual(fizzbuzz.printValue(1), 1)
 
+    def test_printFizz(self):
+        self.assertEqual(fizzbuzz.printValue(3), "Fizz")
+
 if __name__ == '__main__':
     unittest.main()
