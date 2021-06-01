dfa438922c20dfa93e3a734f990897afdfd4b1e3
Jackson Rollins <48576328+JPChaus@users.noreply.github.com>
2021-06-01 00:47:15 -0400

    cleaned up test names

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -8,10 +8,10 @@ class TestCase(unittest.TestCase):
     def test_printFizz(self):
         self.assertEqual(fizzbuzz.printValue(3), "Fizz")
 
-    def test_printFizz(self):
+    def test_printFizz2(self):
         self.assertEqual(fizzbuzz.printValue(5), "Buzz")
     
-    def test_printFizz(self):
+    def test_printFizz3(self):
         self.assertEqual(fizzbuzz.printValue(15), "FizzBuzz")
 
 if __name__ == '__main__':
